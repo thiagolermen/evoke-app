0c6ead91f4b4dc52fccf82b82751bf1cae700824
Thiago Sotoriva Lermen <thiago.lermen@gmail.com>
2022-01-03 00:14:11 -0300

    added Edit, Delete and Like functionalities

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,7 @@
-import PostMessage from "../models/postMessage.js";
+import express from 'express';
+import mongoose from 'mongoose';
+
+import PostMessage from '../models/postMessage.js';
 
 export const getPosts = async (req, res) => {
     try{
@@ -8,7 +11,7 @@ export const getPosts = async (req, res) => {
     } catch(error){
         res.status(404).json({ message: error.messahe });
     }
-}
+};
 
 export const createPost = async (req, res) => {
     const post = req.body;
@@ -21,4 +24,39 @@ export const createPost = async (req, res) => {
     } catch (error) {
         res.status(409).json({ message: error.message });
     }
+};
+
+export const updatePost = async (req, res) => {
+    const { id } = req.params;
+    const { title, message, creator, selectedFile, tags } = req.body;
+    
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+    const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
+
+    await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
+
+    res.json(updatedPost);
+};
+
+export const deletePost = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+
+    await PostMessage.findByIdAndRemove(id);
+
+    res.json({ message: 'Post deleted successfully' });
+};
+
+export const likePost = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
+    
+    const post = await PostMessage.findById(id);
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+    
+    res.json(updatedPost);
 }
